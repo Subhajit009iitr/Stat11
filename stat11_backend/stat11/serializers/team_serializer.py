9f55e201cc659dd23f6fa35c88d743abc054ea8c
Akhil Punia <ak216punia@gmail.com>
2023-03-17 22:43:35 +0530

    Modify serializers

--- stat11_backend/stat11/serializers/team_serializer.py
+++ stat11_backend/stat11/serializers/team_serializer.py
@@ -1,6 +1,5 @@
-from dataclasses import fields
 from rest_framework import serializers
-from models.models import *
+from ..models.team import Team
 
 
 class TeamSerializer(serializers.ModelSerializer):
