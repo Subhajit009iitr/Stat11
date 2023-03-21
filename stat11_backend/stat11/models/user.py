f2881a3ba72d416515282937e6ace3a4c381fa72
Akhil Punia <ak216punia@gmail.com>
2023-03-22 02:10:33 +0530

    Replace is_admin by is_staff field in models

--- stat11_backend/stat11/models/user.py
+++ stat11_backend/stat11/models/user.py
@@ -3,5 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     photo = models.ImageField(max_length=255, blank=True, null=True)
-    is_admin = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
