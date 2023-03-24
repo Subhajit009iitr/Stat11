5d6f25e4a12642e33c09735db32ec250350ded40
Akhil Punia <ak216punia@gmail.com>
2023-03-25 00:07:59 +0530

    Work on login page
    Add images assets
    Work on login page

--- stat11_frontend/src/index.js
+++ stat11_frontend/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ThemeProvider, createTheme, alpha } from '@mui/material';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -14,16 +14,49 @@ const root = createRoot(container);
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFFFFF"
+      main: "#448791",
+      light: alpha("#448791", 0.1)
     },
     secondary: {
-      main: "#448791"
+      main: "#6498E6"
+    },
+    hint: {
+      main: "#7D7D7D",
+      ligth: "#A5A5A5",
+      dark: "#7A7979"
+    },
+    background: {
+      paper: "#FFFFFF",
+      default: "#FFFFFF"
     }
   },
   typography: {
-    h1: {
-      fontSize: "2rem",
-      fontWeight: 400
+    htmlFontSize: 10,
+    h2: {
+      fontSize: "3.2rem",
+      fontWeight: 400,
+    },
+    h3: {
+      fontSize: "2.8rem"
+    },
+    h4: {
+      fontSize: "2.4rem"
+    },
+    h5: {
+      fontSize: "2.2rem"
+    },
+    h6: {
+      fontSize: "2rem"
+    },
+    body1: {
+      fontSize: "1.8rem"
+    },
+    body2: {
+      fontSize: "1.6rem"
+    },
+    button: {
+      fontSize: "2.4rem",
+      fontWeight: 500
     }
   }
 })
--- stat11_frontend/src/pages/login.jsx
+++ stat11_frontend/src/pages/login.jsx
@@ -1,8 +1,111 @@
 import React from 'react'
+import { Box, Link, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import logo from '../assets/Logo.svg'
+import batter from '../assets/Batter.svg'
 
 function Login() {
+  const navigate = useNavigate()
+
+  const centerAlignBoxProps = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+
   return (
-    <div>Login</div>
+    <Box
+    sx={{...{
+      height: "100vh",
+      backgroundColor: "primary.main",
+    },
+    ...centerAlignBoxProps}}
+    >
+      <Box
+      sx={{...{
+        width: "55%",
+        height: "100%",
+        backgroundColor: "transparent",
+      },
+      ...centerAlignBoxProps}}
+      >
+        <Box
+        component="img"
+        sx={{
+          width: "87.5%"
+        }}
+        alt="Batter"
+        src={batter}
+        />
+      </Box>
+      <Box
+      sx={{
+        width: "45%",
+        height: "100%",
+        backgroundColor: "background.default",
+        borderRadius: "2.4rem 0 0 2.4rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+      >
+        <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          width: "100%",
+          height: "7.5%",
+          marginTop: "4rem"
+        }}
+        >
+          <Box
+          component="img"
+          sx={{
+            height: "100%",
+            marginRight: "4rem"
+          }}
+          alt="Logo"
+          src={logo}
+          />
+        </Box>
+        <Box>Form</Box>
+        <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          paddingBottom: "4rem",
+        }}
+        >
+          <Typography
+          variant='h6'
+          color='hint.main'
+          sx={{
+            marginRight: "0.4rem"
+          }}
+          >
+            New User?
+          </Typography>
+          <Link
+          component="button"
+          onClick={() => navigate('/signup')}
+          >
+            <Typography
+            variant='h6'
+            color='hint.main'
+            sx={{
+              marginRight: "4rem"
+            }}
+            >
+              Sign Up
+            </Typography>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
