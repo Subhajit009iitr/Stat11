1a3c64051d266858a6c419df23964eccd86a6aa6
nishita2104 <nishita2104@gmail.com>
2023-04-12 17:29:32 +0530

    add about us/help page

--- stat11_frontend/src/App.js
+++ stat11_frontend/src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home';
-import Auth from './pages/auth';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Auth from "./pages/auth";
+import Help from "./pages/help";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route 
-        path={`/`}
-        element={<Home />}
-        />
-        <Route 
-        path={`/home`}
-        element={<Home />}
-        />
-        <Route 
-        path={`/auth`}
-        element={<Auth />}
-        />
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/home`} element={<Home />} />
+        <Route path={`/auth`} element={<Auth />} />
+        <Route path={`/help`} element={<Help />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
