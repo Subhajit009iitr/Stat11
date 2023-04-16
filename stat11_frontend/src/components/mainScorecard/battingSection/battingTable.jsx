ae98dba988dcbb3323696519dfd554583402c6bb
raiwat7 <101309747+raiwat7@users.noreply.github.com>
2023-04-16 18:31:16 +0530

    make backend connections

--- stat11_frontend/src/components/mainScorecard/battingSection/battingTable.jsx
+++ stat11_frontend/src/components/mainScorecard/battingSection/battingTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableBody,
@@ -6,11 +6,12 @@ import {
   TableRow,
   TableHead,
   TableContainer,
-  Box
+  Box,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { tableCellClasses } from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.secondary.light,
@@ -32,54 +33,86 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createBatterTable(
-  Batter,
-  runs,
-  balls,
-  s4,
-  s6,
-  SR,
-  status,
-  fielder,
-  bowler,
-  howOut
-) {
-  if (status)
-    return { Batter, runs, balls, s4, s6, SR, fielder, bowler, howOut, status };
-  return { Batter, runs, balls, s4, s6, SR, status };
-}
-
-const rows_batter = [
-  createBatterTable(
-    "Virat Kohli",
-    61,
-    44,
-    4,
-    4,
-    138.64,
-    true,
-    "Marcus Stoinis",
-    "Amit Mishra",
-    2
-  ),
-  createBatterTable("Faf du Plessis", 79, 46, 5, 5, 171.74, false),
-  createBatterTable(
-    "Glenn Maxwell",
-    59,
-    29,
-    3,
-    6,
-    203.45,
-    true,
-    "",
-    "Mark Wood",
-    1
-  ),
-  createBatterTable("Dinesh Karthik", 1, 1, 0, 0, 100, false),
-  // createBatterTable("3.Manashree Eclair", 6.0, 8, 1, 0, 5.5),
-];
-
 export default function BattingTable(props) {
+  function createBatterTable(
+    Batter,
+    runs,
+    balls,
+    s4,
+    s6,
+    SR,
+    status,
+    fielder,
+    bowler,
+    howOut
+  ) {
+    if (status)
+      return {
+        Batter,
+        runs,
+        balls,
+        s4,
+        s6,
+        SR,
+        fielder,
+        bowler,
+        howOut,
+        status,
+      };
+    return { Batter, runs, balls, s4, s6, SR, status };
+  }
+
+  const [rows_batter, setRows_batter] = useState([]);
+  function addBatterRow() {
+    setRows_batter([
+      ...rows_batter,
+      createBatterTable(
+        "Virat Kohli",
+        61,
+        44,
+        4,
+        4,
+        138.64,
+        true,
+        "Marcus Stoinis",
+        "Amit Mishra",
+        2
+      ),
+    ]);
+  }
+  useEffect(() => {
+    addBatterRow();
+  }, []);
+  // const rows_batter = [
+  //   createBatterTable(
+  // "Virat Kohli",
+  // 61,
+  // 44,
+  // 4,
+  // 4,
+  // 138.64,
+  // true,
+  // "Marcus Stoinis",
+  // "Amit Mishra",
+  // 2
+  //   ),
+  //   createBatterTable("Faf du Plessis", 79, 46, 5, 5, 171.74, false),
+  //   createBatterTable(
+  //     "Glenn Maxwell",
+  //     59,
+  //     29,
+  //     3,
+  //     6,
+  //     203.45,
+  //     true,
+  //     "",
+  //     "Mark Wood",
+  //     1
+  //   ),
+  //   createBatterTable("Dinesh Karthik", 1, 1, 0, 0, 100, false),
+  //   // createBatterTable("3.Manashree Eclair", 6.0, 8, 1, 0, 5.5),
+  // ];
+
   let secondarytext;
   if (props.hasInningsEnded) {
     secondarytext = "Not out";
@@ -103,13 +136,13 @@ export default function BattingTable(props) {
     }
   };
   return (
-    <Box >
+    <Box>
       <TableContainer
         component={Paper}
-        sx = {{borderTopLeftRadius: "16px", borderTopRightRadius: "16px"}}
+        sx={{ borderTopLeftRadius: "16px", borderTopRightRadius: "16px" }}
       >
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead >
+          <TableHead>
             <TableRow>
               <StyledTableCell sx={{ paddingLeft: "48px" }}>
                 Batter Name
