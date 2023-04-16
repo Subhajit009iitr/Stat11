b420cc9ab32e33d12a93e0ac275d50dd0348fe1a
raiwat7 <101309747+raiwat7@users.noreply.github.com>
2023-04-16 11:22:48 +0530

    connect database

--- stat11_backend/stat11/models/team.py
+++ stat11_backend/stat11/models/team.py
@@ -2,10 +2,16 @@ from django.db import models
 from .match import Match
 from .player import Player
 
+
 class Team(models.Model):
-    match=models.ForeignKey(Match, on_delete=models.CASCADE)
-    name=models.CharField(max_length=255 ,unique=True)
-    players=models.ManyToManyField(Player)
-    flag=models.ImageField(max_length=255, blank=True, null=True)
-    toss=models.BooleanField(default=False)
-    turn=models.BooleanField(default=False)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    players = models.ManyToManyField(Player)
+    flag = models.ImageField(max_length=255, blank=True, null=True)
+    toss = models.BooleanField(default=False)
+    turn = models.BooleanField(default=False)
+    college = models.CharField(max_length=255, blank=True)
+    noball = models.IntegerField(default=0)
+    legbye = models.IntegerField(default=0)
+    bye = models.IntegerField(default=0)
+    wide = models.IntegerField(default=0)
