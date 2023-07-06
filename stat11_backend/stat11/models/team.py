5a4c3de321596b9f15e6758c16cf488296bce9a7
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-07-06 14:41:13 +0530

    All Done

--- stat11_backend/stat11/models/team.py
+++ stat11_backend/stat11/models/team.py
@@ -2,7 +2,6 @@ from django.db import models
 from .match import Match
 from .player import Player
 
-
 class Team(models.Model):
     match=models.ForeignKey(Match, on_delete=models.CASCADE)
     name=models.CharField(max_length=255)
@@ -11,7 +10,7 @@ class Team(models.Model):
     toss=models.BooleanField(default=False)
     turn=models.BooleanField(default=False)
     college=models.CharField(max_length=255,blank=True, null=True)
-    no_ball=models.IntegerField(default=0)
+    noball=models.IntegerField(default=0)
     legbye=models.IntegerField(default=0)
     bye=models.IntegerField(default=0)
     wide=models.IntegerField(default=0)
